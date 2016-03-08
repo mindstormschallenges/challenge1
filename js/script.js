fad8bdf3edb4d0e254902715bf3af8b7c0914b6f
Juan Antonio Breña Moral <bren@juanantonio.info>
2016-03-08 09:57:37 +0100

    https://github.com/mindstormschallenges/challenge1/issues/1

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@
 	// Thus the launchDay below denotes 7 May, 2014.
 	
 var newYear = new Date(); 
-newYear = new Date(2016, 3, 15);
+newYear = new Date(2016, 2, 15);
 $('.defaultCountdown').countdown({until: newYear, format: 'DHMS'}); 
 
 
